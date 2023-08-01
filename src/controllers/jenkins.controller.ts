7bc07d51dfbd42cc4c6355da8283d4f7715763fd
ohmprakash1506 <ohmprakash96@outlook.com>
2023-08-01 15:38:10 +0530

    create API doen for yet to implement

--- src/controllers/jenkins.controller.ts
+++ src/controllers/jenkins.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import axios, { AxiosResponse } from "axios";
-import fs from 'fs';
+import fs from "fs";
 import "dotenv/config";
 
 require("dotenv").config();
 const jenkinsUrl = process.env.JENKINS_URL;
 const username: any = process.env.JENKINS_USER;
 const password: any = process.env.JENKINS_PASS;
+const header = {
+  "Content-Type": "application/json",
+};
 
 export default class jenkins {
-   allJobs = async (req: Request, res: Response) => {
+  allJobs = async (req: Request, res: Response) => {
     const jenkinsJobAPI = `${jenkinsUrl}api/json?pretty=true`;
     console.log(`Jenkins URL:`, jenkinsJobAPI);
     try {
@@ -19,8 +22,6 @@ export default class jenkins {
           password,
         },
       });
-
-      const responseData = response.data.jobs;
 
       console.log(response.data.jobs);
       res.status(200).json(response.data.jobs);
@@ -35,18 +36,42 @@ export default class jenkins {
 
   createJob = async (req: Request, res: Response) => {
     try {
-        const jobName = req.body.jobName;
-        const jobCongifJson = JSON.parse(fs.readFileSync('create.config.json','utf-8'))
-        
-    } catch (error) {}
+      const jobName = req.body.jobName;
+      const createJobUrl = `${jenkinsUrl}createItem?name=${jobName}`;
+      const filePath =
+        "D:\\code\\Jenkins\\jenkins-api\\src\\confilgFile\\createConfig.json";
+      let readData;
+      fs.readFile(filePath, "utf-8", async (error, data) => {
+        if (error) {
+          console.error(`Error reading the file :`, error);
+        }
+        console.log(`readFileData :`, data);
+        readData = JSON.parse(data);
+
+        const jobCongifJson = readData;
+        console.log(`Job: `, jobCongifJson);
+
+        const response = await axios.post(createJobUrl, jobCongifJson, {
+          auth: {
+            username,
+            password,
+          },
+          headers: header,
+        });
+
+        console.log(`JOB : ${jobName} created successfully..!`);
+        console.log(`RESPONSE :`, response.data);
+        res.status(200).json({ message: `Job Created successfully` });
+      });
+
+      
+    } catch (error) {
+      // console.error(`Error Created:`, error);
+      res.status(500).json({ error: `Error creating the job` });
+    }
   };
 
   getJob = async (req: Request, res: Response) => {
-    try {
-    } catch (error) {}
-  };
-
-  updateJob = async (req: Request, res: Response) => {
     try {
     } catch (error) {}
   };
--- src/routes/jenkins.route.ts
+++ src/routes/jenkins.route.ts
@@ -6,7 +6,6 @@ const jenkinsRoute = new jenkins();
 
 router.get('/jenkins/list', jenkinsRoute.allJobs);
 router.post('/jenkins/create', jenkinsRoute.createJob);
-router.put('/jenkins/update', jenkinsRoute.updateJob);
 router.delete('/jenkins/', jenkinsRoute.deleteJob);
 
 export default router;
