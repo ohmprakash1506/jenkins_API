2b0159d1cff0a0a589b9dffcaca4c148302cfaf9
ohm prakash <ohmprakash@ohms-MacBook-Air.local>
2023-08-15 22:17:56 +0530

    jenkins project file updated and docker setup error fixed

--- src/controllers/jenkins.controller.ts
+++ src/controllers/jenkins.controller.ts
@@ -33,6 +33,87 @@ const xmlJob = `<project>
 </project>
 `;
 
+const FreeStyleXML = `<project>
+<actions/>
+<description>My Freestyle Project</description>
+<keepDependencies>false</keepDependencies>
+<properties/>
+<scm class="hudson.scm.NullSCM"/>
+<canRoam>true</canRoam>
+<disabled>false</disabled>
+<blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+<blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+<triggers/>
+<concurrentBuild>false</concurrentBuild>
+<builders>
+  <hudson.tasks.Shell>
+    <command>echo "Hello, Jenkins!"</command>
+  </hudson.tasks.Shell>
+</builders>
+<publishers/>
+<buildWrappers/>
+</project>` 
+
+const pipeLineXML = `<flow-definition plugin="workflow-job@2.40">
+<actions/>
+<description>My Pipeline Project</description>
+<keepDependencies>false</keepDependencies>
+<properties/>
+<definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.85">
+  <script>pipeline {
+  agent any
+  stages {
+      stage('Build') {
+          steps {
+              echo 'Building...'
+          }
+      }
+      stage('Test') {
+          steps {
+              echo 'Testing...'
+          }
+      }
+      stage('Deploy') {
+          steps {
+              echo 'Deploying...'
+          }
+      }
+  }
+}</script>
+  <sandbox>true</sandbox>
+</definition>
+<triggers/>
+<disabled>false</disabled>
+</flow-definition>`
+
+const multiPipeLineXML = `<multibranch-project>
+<actions/>
+<description>My Multibranch Pipeline Project</description>
+<displayName>My Multibranch Pipeline Project</displayName>
+<properties/>
+<sources class="jenkins.branch.MultiBranchProject$BranchSourceList" plugin="branch-api@2.9.0">
+  <data>
+    <jenkins.branch.BranchSource>
+      <source class="jenkins.plugins.git.GitSCMSource" plugin="git@4.10.0">
+        <remote>https://github.com/yourusername/yourrepository.git</remote>
+        <credentialsId>your-git-credentials-id</credentialsId>
+        <traits>
+          <jenkins.plugins.git.traits.BranchDiscoveryTrait/>
+          <jenkins.plugins.git.traits.TagDiscoveryTrait/>
+          <jenkins.plugins.git.traits.ForkPullRequestDiscoveryTrait>
+            <strategyId>1</strategyId>
+          </jenkins.plugins.git.traits.ForkPullRequestDiscoveryTrait>
+        </traits>
+      </source>
+      <strategy class="jenkins.branch.DefaultBranchPropertyStrategy">
+        <properties class="empty-list"/>
+      </strategy>
+    </jenkins.branch.BranchSource>
+  </data>
+</sources>
+<configureBlocks/>
+</multibranch-project>`
+
 export default class jenkins {
   allJobs = async (req: Request, res: Response) => {
     const jenkinsJobAPI = `${jenkinsUrl}api/json?pretty=true`;
@@ -63,7 +144,22 @@ export default class jenkins {
   createJob = async (req: Request, res: Response) => {
     try {
       const jobName = req.body.jobName;
-      const jobConfig = xmlJob;
+      const xmlJobFile = req.body.xmlJobFile;
+
+      let jobConfig;
+
+      if (xmlJobFile === `freestylejob` ) {
+        jobConfig = FreeStyleXML ;
+      } else if ( xmlJobFile === `pipelinejob` ){
+        jobConfig = pipeLineXML;
+      } else if ( xmlJobFile === `multiplipelinejob`){
+        jobConfig = multiPipeLineXML;
+      } else {
+        res.send(`jobconfig deatils not mentioned`);
+      }
+
+      console.log(`jobConfig:`,jobConfig);
+
       const createJobUrl = `${jenkinsUrl}createItem?name=${jobName}`;
 
       const { csrfCrumb, csrfHeader } = await CSRFToken(
